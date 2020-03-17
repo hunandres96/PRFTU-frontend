96b0eb6f0970922ee7715b308c34c6abcc8a1ff1
hunandres96 <hunandres96@hotmail.com>
2020-03-16 21:29:34 -0500

    updated student and studentList components

--- src/components/students/Student.js
+++ src/components/students/Student.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react'
+import { Card, Form, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusSquare, faSave } from '@fortawesome/free-solid-svg-icons'
 
 export default class Student extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: null,
+      email: null,
+      major: null
+    }
+    this.changeStudentState = this.changeStudentState.bind(this);
+    this.submitStudent = this.submitStudent.bind(this);
+
+  }
+
+  changeStudentState(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitStudent(e) {
+    alert(this.state.name)
+    e.preventDefault()
+  }
+
   render() {
     return (
-      <div className="text-white">
-        Add Student
-      </div>
+      <Card className="border border-dark bg-dark text-white">
+        <Card.Header><FontAwesomeIcon icon={faPlusSquare} /> Add new Student</Card.Header>
+        <Form onSubmit={this.submitStudent} id="studentFormId">
+          <Card.Body>
+            <Form.Group controlId="formGridName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control required
+                value={this.state.name}
+                onChange={this.changeStudentState}
+                name="name"
+                type="text"
+                placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control required
+                value={this.state.email}
+                onChange={this.changeStudentState}
+                name="email"
+                type="email"
+                placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formGridMajor">
+              <Form.Label>Major</Form.Label>
+              <Form.Control required
+                value={this.state.major}
+                onChange={this.changeStudentState}
+                name="major"
+                type="text"
+                placeholder="Enter major" />
+            </Form.Group>
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="primary" type="submit">
+              <FontAwesomeIcon icon={faSave} /> Submit
+            </Button>
+          </Card.Footer>
+        </Form>
+      </Card>
     )
   }
 }
--- src/components/students/StudentList.js
+++ src/components/students/StudentList.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
+import { Card, Table } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faList } from '@fortawesome/free-solid-svg-icons'
 
 export default class StudentList extends Component {
   render() {
     return (
-      <div className="text-white">
-        Student List
-      </div>
+      <Card className="border border-dark bg-dark text-white">
+        <Card.Header><FontAwesomeIcon icon={faList} /> Students:</Card.Header>
+        <Card.Body>
+          <Table bordered hover striped variant="dark">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Major</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Andres Hun</td>
+                <td>andreshun@gmail.com</td>
+                <td>Marketing</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Miguel Hun</td>
+                <td>miguelhun@gmail.com</td>
+                <td>Info System</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Jorge Villegas</td>
+                <td>jorgevillegas@gmail.com</td>
+                <td>Graphic Design</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
     )
   }
 }
